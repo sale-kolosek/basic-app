40f2c32819f76c4bab4e63e1a3514c560324de1b
Nebojsa Zoric <office@kolosek.com>
2022-12-04 12:46:47 +0100

    Update index.html.erb_spec.rb

--- spec/views/items/index.html.erb_spec.rb
+++ spec/views/items/index.html.erb_spec.rb
@@ -15,6 +15,7 @@ RSpec.describe "items/index", type: :view do
   it "renders a list of items" do
     render
     cell_selector = Rails::VERSION::STRING >= '7' ? 'div>p' : 'tr>td'
+    
     assert_select cell_selector, text: Regexp.new("Name".to_s), count: 2
   end
 end
