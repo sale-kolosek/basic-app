d2f97c4e4f84059d69f4a86923bfafd6e33f4743
Nesha <nesha@kolosek.com>
2023-10-24 20:48:52 +0200

    rubyci

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -92,3 +92,21 @@ RSpec.configure do |config|
   Kernel.srand config.seed
 =end
 end
+
+
+if ENV["RUBY_CI_SECRET_KEY"]
+  require "rspec/core/runner"
+  require "ruby_ci/runner_prepend"
+
+  class RSpec::Core::ExampleGroup
+    def self.filtered_examples
+      rubyci_scoped_ids = Thread.current[:rubyci_scoped_ids] || ""
+
+      RSpec.world.filtered_examples[self].filter do |ex|
+        rubyci_scoped_ids == "" || /^#{rubyci_scoped_ids}($|:)/.match?(ex.metadata[:scoped_id])
+      end
+    end
+  end
+
+  RSpec::Core::Runner.prepend(RubyCI::RunnerPrepend)
+end
